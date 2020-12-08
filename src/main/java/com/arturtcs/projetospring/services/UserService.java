b80ad959bfe38ade2dc6bd9bd5b0e070d5b583a9
Artur Tarcisio <arturtarcisio1@gmail.com>
2020-12-08 12:01:17 -0300

    Service layer, component registration

---
+++ src/main/java/com/arturtcs/projetospring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.arturtcs.projetospring.services;
+
+import com.arturtcs.projetospring.entities.User;
+import com.arturtcs.projetospring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+       return repository.findAll();
+    }
+
+    public User findUserById(Long id) {
+       Optional<User> user = repository.findById(id);
+       return user.get();
+    }
+
+
+}
